71ffe5d8f9c659e821291d98a79ad5ea49048bdd
deepax <dipakacharya51@gmail.com>
2017-06-15 23:15:05 +0545

    chat page improved

--- public/js/index.js
+++ public/js/index.js
@@ -15,14 +15,45 @@ socket.on('newMessage', function (message) {
     jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function(message){
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target = "_blank">My current Location</a>');
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
+    jQuery('message').append(li);
+});
+
 jQuery('#message-form').on('submit', function(e){
     e.preventDefault();
 
-
+    var messageTextbox = jQuery('[name=message]');
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function(){
-
+        messageTextbox.val('');
     });
 }); 
+
+var locationButton = jQuery('#send-location');
+
+locationButton.on('click', function(){
+    if(!navigator.geolocation){
+        return alert('Geolocation not supported');
+    };
+    locationButton.attr('disabled', 'disabled').text('Sending Location...');
+
+    navigator.geolocation.getCurrentPosition(function (position){
+
+        locationButton.removeAttr('disabled').text('Send Location...');
+      //  console.log(position);
+        socket.emit('createLocationMessage',{ 
+            latitude : position.coords.latitude,
+            longitude : position.coords.longitude
+        });
+    }, function() {
+        locationButton.removeAttr('disabled').text('Send Location...');
+        alert('Unable to fetch location');
+    });
+});
