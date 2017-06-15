dd1c6b5d279a181b54b72f799e997e4bf61e15a9
deepax <dipakacharya51@gmail.com>
2017-06-15 19:58:22 +0545

    added connect and disconnect events

--- server/server.js
+++ server/server.js
@@ -7,6 +7,16 @@ const publicPath = path.join(__dirname + '/../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+
+    socket.on('disconnect', () => {
+        console.log('Client left the chat');
+    });
+});
 
 app.use(express.static(publicPath));
 
